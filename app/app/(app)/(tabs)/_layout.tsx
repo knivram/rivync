54a1cba77a9c6502962730a0a6bd094c59e17fd3
Marvin <marvin.kramer.lol@gmail.com>
2024-04-30 20:09:37 +0200

    added all initial tabs

--- app/app/(app)/(tabs)/_layout.tsx
+++ app/app/(app)/(tabs)/_layout.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import {Tabs} from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
 
 const page = () => {
     return (
         <Tabs >
-            <Tabs.Screen name="activity" options={{title: 'Activity'}} />
-            <Tabs.Screen name="profile" options={{title: 'Profile'}} />
+            <Tabs.Screen name="activity" options={{
+                title: 'Activity',
+                tabBarIcon: ({size, color}) => <Ionicons name='home-outline' size={size} color={color}/>
+            }} />
+            <Tabs.Screen name="events" options={{
+                title: 'Events',
+                tabBarIcon: ({size, color}) => <Ionicons name='list' size={size} color={color}/>
+            }} />
+            <Tabs.Screen name="chat" options={{
+                title: 'Chat',
+                tabBarIcon: ({size, color}) => <Ionicons name='chatbubble-ellipses-outline' size={size} color={color}/>
+            }} />
+            <Tabs.Screen name="profile" options={{
+                title: 'Profile',
+                tabBarIcon: ({size, color}) => <Ionicons name='person-circle-outline' size={size} color={color}/>
+            }} />
         </Tabs>
     );
 };
