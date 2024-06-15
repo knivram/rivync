ec080b45357f557b82394d13114a1809b3693799
Marvin <marvin.kramer.lol@gmail.com>
2024-06-15 19:07:37 +0200

    #22 removed bidirectional scrolling

--- app/app/(app)/(tabs)/events.tsx
+++ app/app/(app)/(tabs)/events.tsx
@@ -1,9 +1,8 @@
-import React, {useState, useRef, useEffect} from 'react';
-import {View, FlatList as NativeFlatList} from "react-native";
+import React, {useState, useEffect} from 'react';
+import {View, FlatList, Text, ActivityIndicator} from "react-native";
 import {useTeam} from "@/provider/TeamProvider";
 import {Event, EventType} from "@/lib/types/event";
 import EventCard from "@/components/EventCard";
-import {FlatList} from 'react-native-bidirectional-infinite-scroll';
 import moment from '@/lib/moments';
 import {supabase} from "@/lib/supabase";
 
@@ -15,16 +14,13 @@ const EventPage = () => {
     const [events, setEvents] = useState<Event[]>([]);
     const [futureEventPage, setFutureEventPage] = useState(1);
     const [hasMoreFutureEvents, setHasMoreFutureEvents] = useState(true);
-    const [pastEventPage, setPastEventPage] = useState(1);
-    const [hasMorePastEvents, setHasMorePastEvents] = useState(true);
+    const [initialLoaded, setInitialLoaded] = useState(false);
 
     const fetchFutureEvents = async () => {
-        console.log('old future event page', futureEventPage)
         if (!hasMoreFutureEvents) {
             return;
         }
         const offset = (futureEventPage - 1) * PAGE_SIZE;
-        console.log('future offset', offset)
         const {data, error} = await supabase
             .from('event')
             .select()
@@ -42,7 +38,6 @@ const EventPage = () => {
                 start: moment(item.start),
                 end: moment(item.end),
             }));
-            console.log('future data', data)
             setEvents((prevEvents) => [...prevEvents, ...formattedData]);
             setFutureEventPage((prevPage) => prevPage + 1);
             if (formattedData.length !== PAGE_SIZE) {
@@ -51,54 +46,23 @@ const EventPage = () => {
         }
     }
 
-    const fetchPastEvents = async () => {
-        console.log('old past event page', pastEventPage)
-        if (!hasMorePastEvents) {
-            return;
-        }
-        const offset = (pastEventPage - 1) * PAGE_SIZE;
-        console.log('past offset', offset)
-        const {data, error} = await supabase
-            .from('event')
-            .select()
-            .eq('team_id', team?.id)
-            .lt('start', currentDate.toISOString())
-            .order('start', {ascending: false}) // Fetch in descending order to reverse later
-            .range(offset, offset + PAGE_SIZE - 1);
-
-        if (error) {
-            console.error(error);
-        } else {
-            const formattedData = data.map((item) => ({
-                ...item,
-                type: item.type as EventType,
-                start: moment(item.start),
-                end: moment(item.end),
-            })).reverse();
-            console.log('past data', data)
-            setEvents((prevEvents) => [...formattedData, ...prevEvents]);
-            setPastEventPage((prevPage) => prevPage + 1);
-            if (formattedData.length !== PAGE_SIZE) {
-                setHasMorePastEvents(false);
-            }
-        }
-    }
-
     useEffect(() => {
-        fetchFutureEvents()
+        fetchFutureEvents().then(() =>
+            setInitialLoaded(true)
+        )
     }, []);
 
     return (
         <View style={{flex: 1, paddingHorizontal: 8}}>
-            <FlatList
-                data={events}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}: { item: Event }) => <EventCard event={item}/>}
-                onStartReached={fetchPastEvents}
-                onStartReachedThreshold={0.1}
-                onEndReached={fetchFutureEvents}
-                onEndReachedThreshold={0.1}
-            />
+            {initialLoaded &&
+                <FlatList
+                    data={events}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}: { item: Event }) => <EventCard event={item}/>}
+                    onEndReached={fetchFutureEvents}
+                    ListFooterComponent={hasMoreFutureEvents ? <ActivityIndicator /> : null}
+                />
+            }
         </View>
     );
 };
