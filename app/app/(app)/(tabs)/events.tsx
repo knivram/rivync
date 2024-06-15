866a5d5c54be1e89a85f06732f0cd4bb68c6bee4
Marvin <marvin.kramer.lol@gmail.com>
2024-06-15 18:54:34 +0200

    #22 implemented own event list with a scroll view library for loading past events. not working

--- app/app/(app)/(tabs)/events.tsx
+++ app/app/(app)/(tabs)/events.tsx
@@ -1,80 +1,37 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { View, FlatList, ActivityIndicator } from "react-native";
-import { supabase } from "@/lib/supabase";
-import { useTeam } from "@/provider/TeamProvider";
-import moment from '@/lib/moments';
-import { Event, EventType } from "@/lib/types/event";
+import React, {useState, useRef, useEffect} from 'react';
+import {View, FlatList as NativeFlatList} from "react-native";
+import {useTeam} from "@/provider/TeamProvider";
+import {Event, EventType} from "@/lib/types/event";
 import EventCard from "@/components/EventCard";
+import {FlatList} from 'react-native-bidirectional-infinite-scroll';
+import moment from '@/lib/moments';
+import {supabase} from "@/lib/supabase";
 
 const PAGE_SIZE = 10;
 
 const EventPage = () => {
-    const { team } = useTeam();
+    const currentDate = moment()
+    const {team} = useTeam();
     const [events, setEvents] = useState<Event[]>([]);
-    const [loading, setLoading] = useState(false);
-    const [initialIndex, setInitialIndex] = useState<number | null>(null);
-    const flatListRef = useRef<FlatList>(null);
+    const [futureEventPage, setFutureEventPage] = useState(1);
+    const [hasMoreFutureEvents, setHasMoreFutureEvents] = useState(true);
+    const [pastEventPage, setPastEventPage] = useState(1);
+    const [hasMorePastEvents, setHasMorePastEvents] = useState(true);
 
-    useEffect(() => {
-        if (team?.id) {
-            fetchInitialEvents();
+    const fetchFutureEvents = async () => {
+        console.log('old future event page', futureEventPage)
+        if (!hasMoreFutureEvents) {
+            return;
         }
-    }, [team]);
-
-    const fetchInitialEvents = async () => {
-        setLoading(true);
-        const currentDate = moment().toISOString();
-
-        const { data: upcomingData, error: upcomingError } = await supabase
+        const offset = (futureEventPage - 1) * PAGE_SIZE;
+        console.log('future offset', offset)
+        const {data, error} = await supabase
             .from('event')
             .select()
             .eq('team_id', team?.id)
-            .gte('start', currentDate)
-            .order('start', { ascending: true })
-            .limit(PAGE_SIZE);
-
-        const { data: pastData, error: pastError } = await supabase
-            .from('event')
-            .select()
-            .eq('team_id', team?.id)
-            .lt('start', currentDate)
-            .order('start', { ascending: false }) // Fetch in descending order to reverse later
-            .limit(PAGE_SIZE);
-
-        if (upcomingError || pastError) {
-            console.error(upcomingError || pastError);
-        } else {
-            const formattedUpcomingData = upcomingData.map((item) => ({
-                ...item,
-                type: item.type as EventType,
-                start: moment(item.start),
-                end: moment(item.end),
-            }));
-            const formattedPastData = pastData.map((item) => ({
-                ...item,
-                type: item.type as EventType,
-                start: moment(item.start),
-                end: moment(item.end),
-            }));
-            setEvents([...formattedPastData.reverse(), ...formattedUpcomingData]);
-            setInitialIndex(formattedPastData.length + 6);
-        }
-
-        setLoading(false);
-    };
-
-    const fetchMoreEvents = async (direction: 'newer' | 'older') => {
-        setLoading(true);
-        const lastEvent = direction === 'newer' ? events[events.length - 1] : events[0];
-        const { start } = lastEvent;
-
-        const { data, error } = await supabase
-            .from('event')
-            .select()
-            .eq('team_id', team?.id)
-            .filter('start', direction === 'newer' ? 'gt' : 'lt', start.toISOString())
-            .order('start', { ascending: direction === 'newer' })
-            .limit(PAGE_SIZE);
+            .gte('start', currentDate.toISOString())
+            .order('start', {ascending: true})
+            .range(offset, offset + PAGE_SIZE - 1);
 
         if (error) {
             console.error(error);
@@ -85,60 +42,62 @@ const EventPage = () => {
                 start: moment(item.start),
                 end: moment(item.end),
             }));
-
-            setEvents(prevEvents => direction === 'newer'
-                ? [...prevEvents, ...formattedData]
-                : [...formattedData.reverse(), ...prevEvents]);
+            console.log('future data', data)
+            setEvents((prevEvents) => [...prevEvents, ...formattedData]);
+            setFutureEventPage((prevPage) => prevPage + 1);
+            if (formattedData.length !== PAGE_SIZE) {
+                setHasMoreFutureEvents(false);
+            }
         }
+    }
 
-        setLoading(false);
-    };
-
-    const loadMoreEvents = () => {
-        if (!loading) {
-            fetchMoreEvents('newer');
+    const fetchPastEvents = async () => {
+        console.log('old past event page', pastEventPage)
+        if (!hasMorePastEvents) {
+            return;
         }
-    };
+        const offset = (pastEventPage - 1) * PAGE_SIZE;
+        console.log('past offset', offset)
+        const {data, error} = await supabase
+            .from('event')
+            .select()
+            .eq('team_id', team?.id)
+            .lt('start', currentDate.toISOString())
+            .order('start', {ascending: false}) // Fetch in descending order to reverse later
+            .range(offset, offset + PAGE_SIZE - 1);
 
-    const loadOlderEvents = () => {
-        if (!loading) {
-            fetchMoreEvents('older');
+        if (error) {
+            console.error(error);
+        } else {
+            const formattedData = data.map((item) => ({
+                ...item,
+                type: item.type as EventType,
+                start: moment(item.start),
+                end: moment(item.end),
+            })).reverse();
+            console.log('past data', data)
+            setEvents((prevEvents) => [...formattedData, ...prevEvents]);
+            setPastEventPage((prevPage) => prevPage + 1);
+            if (formattedData.length !== PAGE_SIZE) {
+                setHasMorePastEvents(false);
+            }
         }
-    };
+    }
 
-    const renderFooter = () => {
-        if (!loading) return null;
-        return <ActivityIndicator style={{ margin: 20 }} />;
-    };
-
-    const getItemLayout = (_: unknown, index: number) => (
-        { length: 70, offset: 70 * index, index }
-    );
-
-    const onScrollToIndexFailed = ({ index }: { index: number }) => {
-        const wait = new Promise(resolve => setTimeout(resolve, 500));
-        wait.then(() => {
-            flatListRef.current?.scrollToIndex({ index: index, animated: true });
-        });
-    };
+    useEffect(() => {
+        fetchFutureEvents()
+    }, []);
 
     return (
-        <View style={{ flex: 1, paddingHorizontal: 8 }}>
+        <View style={{flex: 1, paddingHorizontal: 8}}>
             <FlatList
-                ref={flatListRef}
                 data={events}
                 keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }: { item: Event }) => <EventCard event={item} />}
-                onEndReached={loadMoreEvents}
+                renderItem={({item}: { item: Event }) => <EventCard event={item}/>}
+                onStartReached={fetchPastEvents}
+                onStartReachedThreshold={0.1}
+                onEndReached={fetchFutureEvents}
                 onEndReachedThreshold={0.1}
-                ListFooterComponent={renderFooter}
-                ListHeaderComponent={renderFooter}
-                initialScrollIndex={initialIndex}
-                getItemLayout={getItemLayout}
-                onScrollToIndexFailed={onScrollToIndexFailed}
-                onRefresh={loadOlderEvents}
-                refreshing={loading}
-                contentContainerStyle={{gap: 8}}
             />
         </View>
     );
