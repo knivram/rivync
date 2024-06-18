dc826eccf6a0aa4f8c1509b0909166d3ce01edb4
Marvin <marvin.kramer.lol@gmail.com>
2024-06-18 10:09:34 +0200

    [#30] created component for text, title and container

---
+++ webapp/src/components/ui/text.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import {cn} from "@/lib/utils";
+
+
+const Text = React.forwardRef<
+    HTMLParagraphElement,
+    React.HTMLAttributes<HTMLParagraphElement>
+>(({ className, ...props }, ref) => (
+    <p ref={ref} className={cn("text-base text-gray-700", className)} {...props} />
+));
+
+Text.displayName = 'Text';
+
+
+const Title = React.forwardRef<
+    HTMLHeadingElement,
+    React.HTMLAttributes<HTMLHeadingElement>
+>(({ className, ...props }, ref) => (
+    <h1 ref={ref} className={cn("text-3xl font-bold", className)} {...props} />
+));
+
+Title.displayName = 'Title';
+
+export { Text, Title };
